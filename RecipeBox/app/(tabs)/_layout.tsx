6d9d13c8411d5dfb503b5c6365efde6fb80f93cc
Juha Duong <juha.duong@edu.turkuamk.fi>
2025-12-18 23:46:49 +0200

    made the _layout.tsx bottom navbar more readable.

--- RecipeBox/app/(tabs)/_layout.tsx
+++ RecipeBox/app/(tabs)/_layout.tsx
@@ -1,33 +1,45 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
-
-import { HapticTab } from '@/components/haptic-tab';
-import { IconSymbol } from '@/components/ui/icon-symbol';
-import { Colors } from '@/constants/theme';
-import { useColorScheme } from '@/hooks/use-color-scheme';
+import { Tabs } from "expo-router";
+import { HapticTab } from "@/components/haptic-tab";
+import { Platform } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+import { lightTheme } from "@/styles/theme";
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+        tabBarActiveTintColor: lightTheme.colors.primary,
         headerShown: false,
         tabBarButton: HapticTab,
-      }}>
+        tabBarStyle: Platform.select({
+          ios: {
+            position: "absolute",
+          },
+          default: {},
+        }),
+        tabBarLabelStyle: {
+          fontSize: 16,
+          fontWeight: "600",
+          marginBottom: 4,
+        },
+      }}
+    >
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Search',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="magnifyingglass" color={color} />,
+          title: "Search",
+          tabBarIcon: ({ color }) => (
+            <Ionicons name="search" size={24} color={color} />
+          ),
         }}
       />
       <Tabs.Screen
         name="favorites"
         options={{
-          title: 'Favorites',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="heart.fill" color={color} />,
+          title: "Favorites",
+          tabBarIcon: ({ color }) => (
+            <Ionicons name="heart" size={24} color={color} />
+          ),
         }}
       />
     </Tabs>
